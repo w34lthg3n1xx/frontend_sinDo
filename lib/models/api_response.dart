241d3db3a985c94f223b6baaa0dd5afec71c7078
w34lthg3n1xx <nouss396@gmail.com>
2026-02-25 20:12:38 +0100

    update api responses format

--- lib/models/api_response.dart
+++ lib/models/api_response.dart
@@ -14,9 +14,16 @@ class ApiResponse {
   factory ApiResponse.fromJson(Map<String, dynamic> json) {
     return ApiResponse(
       qualification: json['qualification'] as String? ?? 'N/A',
-      articles: json['articles'] as String? ?? 'N/A',
-      risques: json['risques'] as String? ?? 'N/A',
-      conseils: json['conseils'] as String? ?? 'N/A',
+
+      // On transforme la liste en texte lisible
+      articles: (json['applicable_articles'] as List<dynamic>?)
+              ?.map((e) => e.toString())
+              .join('\n\n') ??
+          'N/A',
+
+      risques: json['risks'] as String? ?? 'N/A',
+
+      conseils: json['advice'] as String? ?? 'N/A',
     );
   }
 }
